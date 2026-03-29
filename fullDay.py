7b2d50aa4dc00ef95433e3d15678212e0ca3cf12
660660mx <6606660mx@gmail.com>
2026-03-29 18:31:42 +0500

    commit

--- fullDay.py
+++ fullDay.py
@@ -9,4 +9,4 @@ for i in range(1,365):
         os.system('git add .')
         os.system('git commit --date="' + d + '" -m "commit"')
 
-os.system('git push origin master')
+os.system('git push origin main')
